8ce2d1cbbac38f8e2f9bfeefc772c499920945e2
Thallys Braz <thallysbraz3@gmail.com>
2024-10-16 18:49:28 -0300

    feat: commit adicionando codigo

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,11 +3,34 @@ const userService = require('../services/userService');
 const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
-
+    try {
+        const { email, name, password } = req.body;
+        const user = await userService.registerUser(email, name, password);
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(500).send({
+            error: "Registration failed",
+            details: error.message
+        })
+    }
 };
 
 const login = async (req, res) => {
-
+    try {
+        const { email, password } = req.body;
+        const user = await userService.authenticateUser(email, password);
+        if (!user) {
+            res.status(401).send({ error: "Não autorizado!" });
+        }
+        const token = jwt.sign(
+            { id: user.id, email: user.email },
+            "secret",
+            { expiresIn: "3h" }
+        )
+        res.send({ token });
+    } catch (error) {
+        res.status(500).send({ error: "Falha no login!" });
+    }
 };
 
 const getProfile = async (req, res) => {
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,11 +3,22 @@ const userRepository = require('../repository/userRepository');
 const bcrypt = require('bcrypt');
 
 async function registerUser(email, name, password) {
-
+    try {
+        const hasedPassword = await bcrypt.hash(password, 10);
+        return await userRepository.createUser(email, name, hasedPassword);
+    } catch (error) {
+        throw error;
+    }
 }
 
 async function authenticateUser(email, password) {
+    const user = await userRepository.findUserByEmail(email);
+    if (!user) {
+        return null;
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
 
+    return isMatch ? user : null;
 }
 
 async function getUserById(id) {
