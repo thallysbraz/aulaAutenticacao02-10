ed02d2fb7b9fa5a341b260fa71fc4a71a8564c5e
Thallys Braz <thallysbraz3@gmail.com>
2025-04-30 20:19:22 -0300

    criando rota de registro e autentication

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,17 @@ const userService = require('../services/userService');
 const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
-
+    try {
+        const { email, name, password } = req.body;
+        const user = await userService.registerUser(email, name, password);
+        res.status(200).send(user);
+    } catch (error) {
+        console.log('Registro deu erro: ', error);
+        res.status(500).send({
+            error: 'Registro falhou',
+            details: error.message
+        })
+    }
 };
 
 const login = async (req, res) => {
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,10 +3,27 @@ const userRepository = require('../repository/userRepository');
 const bcrypt = require('bcrypt');
 
 async function registerUser(email, name, password) {
+    try {
+        console.log("input: ", {
+            email, name, password
+        });
 
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        return await userRepository.createUser(email, name, hashedPassword);
+    } catch (error) {
+        console.log("Error na service registerUser", error);
+        throw error;
+    }
 }
 
 async function authenticateUser(email, password) {
+    const user = await userRepository.findUserByEmail(email);
+    if (!user) {
+        return null;
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    return isMatch ? user : null;
 
 }
 
